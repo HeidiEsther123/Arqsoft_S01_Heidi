6134fbcd89f826a4e7fa2424239b3a075cb49609
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-05-06 08:53:19 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,74 @@
+﻿using Catalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private static List<Item> _items = new()
+        {
+            new Item
+            {
+                Id = 1,
+                Titulo = "Devil May Cry",
+                Genero = "Hack and Slash",
+                Ano = 2001,
+                Consola = "PlayStation 2",
+                Descripcion = "Videojuego que trata de un cazador..."
+            },
+            new Item
+            {
+                Id = 2,
+                Titulo = "Castlevania: Symphony of the Night",
+                Genero = "Metroidvania",
+                Ano = 1997,
+                Consola = "PlayStation",
+                Descripcion = "Videojuego que trata de un cazador..."
+            },
+            new Item
+            {
+                Id = 3,
+                Titulo = "NieR: Automata",
+                Genero = "Action RPG",
+                Ano = 2017,
+                Consola = "PlayStation 4",
+                Descripcion = "Videojuego que combina acción y narrativa filosófica..."
+            }
+        };
+
+      
+        public IActionResult Index(string? genero)
+        {
+            var resultado = string.IsNullOrEmpty(genero)
+                ? _items
+                : _items.Where(i => i.Genero == genero).ToList();
+
+            ViewBag.Generos = _items.Select(i => i.Genero).Distinct().ToList();
+            ViewBag.GeneroActual = genero;
+
+            return View(resultado);
+        }
+
+      
+        public IActionResult Detalle(int id)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            return item == null ? NotFound() : View(item);
+        }
+
+    
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public IActionResult Agregar(Item item)
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
